53594babd9c8205f9e7b4ee8b858ce0be427c923
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2021-01-01 20:27:35 -0600

    Add Event Parsing

---
+++ src/io/InputReader.java
@@ -0,0 +1,147 @@
+package io;
+
+/* Event Grapher
+ * Christopher Cruzen
+ * 01.01.2021
+ *
+ *   InputReader is the root level input manager for Event Grapher. It reads from the source
+ * data file, parses events, and returns usable object lists to its calling class.
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class InputReader {
+
+
+    /*--- Variable Declarations ---*/
+
+    // Constants
+    private static final String INPUT_FILENAME = "input\\input.txt";
+    private static final SimpleDateFormat EVENT_STRING_FORMAT = new SimpleDateFormat("MM.dd.yyyy - hh:mma");
+
+    // Variables
+    private File inputFile;
+    private List<String> inputContents;
+
+
+    /*--- Public Methods ---*/
+
+    public InputReader() {
+
+        // Set Up Variables
+        inputFile = new File(INPUT_FILENAME);
+
+        // Read Input File
+        readInputFile();
+    }
+
+    public List<Date> getFullEventList() {
+
+        // Set Up Local Variables
+        List<Date> eventList = new ArrayList<>();
+
+        // Parse Events
+        try {
+            for (String eventString: inputContents) {
+                eventList.add(EVENT_STRING_FORMAT.parse(eventString));
+            }
+        } catch (ParseException exception) {
+            exception.printStackTrace();
+        }
+
+        return eventList;
+    }
+
+    public List<Date> getSoloEventList() {
+
+        // Set Up Local Variables
+        List<Date> eventList = new ArrayList<>();
+
+        // Parse Events
+        try {
+            for (String eventString: inputContents) {
+                if (!eventString.contains("*")) {
+                    eventList.add(EVENT_STRING_FORMAT.parse(eventString));
+                }
+            }
+        } catch (ParseException exception) {
+            exception.printStackTrace();
+        }
+
+        return eventList;
+    }
+
+    public List<Date> getSharedEventList() {
+
+        // Set Up Local Variables
+        List<Date> eventList = new ArrayList<>();
+
+        // Parse Events
+        try {
+            for (String eventString: inputContents) {
+                if (eventString.contains("**")) {
+                    eventList.add(EVENT_STRING_FORMAT.parse(eventString));
+                }
+            }
+        } catch (ParseException exception) {
+            exception.printStackTrace();
+        }
+
+        return eventList;
+    }
+
+    public List<Date> getVirtualEventList() {
+
+        // Set Up Local Variables
+        List<Date> eventList = new ArrayList<>();
+
+        // Parse Events
+        try {
+            for (String eventString: inputContents) {
+                if (eventString.contains("*") && !eventString.contains("**")) {
+                    eventList.add(EVENT_STRING_FORMAT.parse(eventString));
+                }
+            }
+        } catch (ParseException exception) {
+            exception.printStackTrace();
+        }
+
+        return eventList;
+    }
+
+
+    /*--- Private I/O Methods ---*/
+
+    private void readInputFile() {
+        if (inputFile == null) {
+            System.out.println("Error: No input file.");
+            System.exit(1);
+        }
+
+        inputContents = new ArrayList<>();
+        BufferedReader bufferedReader;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(inputFile));
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                inputContents.add(line);
+                line = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+        } catch (Exception exception) {
+            System.out.println("Error: Issue reading input file.");
+            System.exit(1);
+        }
+    }
+
+}
--- src/ui/GraphPanel.java
+++ src/ui/GraphPanel.java
@@ -1,21 +1,22 @@
 package ui;
 
-import ui.utility.DisplayUtility;
+import io.InputReader;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
-import java.util.ArrayList;
+import java.text.DecimalFormat;
 import java.util.Date;
+import java.util.List;
 
 /* Event Grapher
  * Christopher Cruzen
  * 01.01.2021
  *
  *   GraphPanel is backbone of Event Grapher. It delegates dataset parsing to a utility IO
- * class, stores the retrieved event list, and uses that data to draw infographics to the
- * screen.
+ * class, stores the retrieved event lists, and uses that data to draw stats and infographics
+ * to the screen.
  */
 
 class GraphPanel extends JPanel {
@@ -27,19 +28,38 @@ class GraphPanel extends JPanel {
     private static final int POINT_DIAMETER = 11;
     private static final int POINT_DIAMETER_SMALL = 5;
     private static final int POINT_OFFSET = 0;
+
     private static final Color BACKGROUND_COLOR = new Color(255, 255, 255);
-    private static final Color TEXT_COLOR = new Color(0, 0, 0);
+    private static final Color TEXT_COLOR_PRIMARY = new Color(0, 0, 0);
+    private static final Color TEXT_COLOR_SECONDARY = new Color(80, 80, 80);
+    private static final int WINDOW_PADDING = 15;
+    private static final int TEXT_SIZE = 15;
+    private static final int TEXT_LINE_SPACING = 2;
+    private static final int TEXT_INDENT = 10;
+    private static final int TEXT_COLUMN = 30;
+
+    private DecimalFormat DECIMAL_FORMAT_3 = new DecimalFormat("0.00#");
+    private DecimalFormat DECIMAL_FORMAT_P = new DecimalFormat("0.#");
 
     // Variables
+    GraphWindow parent;
+    InputReader inputReader;
+    List<Date> fullEventList;
+    List<Date> soloEventList;
+    List<Date> sharedEventList;
+    List<Date> virtualEventList;
 
 
     /*--- Constructor ---*/
 
     GraphPanel() {
 
-        // Initialize Variables
-
-        // Store Event Data
+        // Parse & Store Event Data
+        inputReader = new InputReader();
+        fullEventList = inputReader.getFullEventList();
+        soloEventList = inputReader.getSoloEventList();
+        sharedEventList = inputReader.getSharedEventList();
+        virtualEventList = inputReader.getVirtualEventList();
 
         // Configure UI
         setBackground(BACKGROUND_COLOR);
@@ -56,19 +76,89 @@ class GraphPanel extends JPanel {
         Graphics2D graphics = (Graphics2D) g;
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // TODO - Draw Graphs
-        graphics.setColor(TEXT_COLOR);
-        graphics.drawString(
-                "Hello World!",
-                DisplayUtility.getWindowCenterX(),
-                DisplayUtility.getWindowCenterY()
-        );
+        // Draw Interface
+        if (fullEventList != null && sharedEventList != null && virtualEventList != null) {
+            drawTotals(graphics);
+        } else {
+            invalidate();
+            repaint();
+        }
     }
 
 
-    /*--- Private Data Methods ---*/
+    /*--- Private Draw Methods ---*/
+
+    private void drawTotals(Graphics2D graphics) {
+
+        // Set Up Local Variables
+        int currentHeight = WINDOW_PADDING * 2;
+
+        // Title
+        graphics.setColor(TEXT_COLOR_PRIMARY);
+        graphics.setFont(new Font ("Sanserif", Font.BOLD, TEXT_SIZE));
+        graphics.drawString(
+                "2020 Events",
+                WINDOW_PADDING,
+                currentHeight
+        );
+        currentHeight += graphics.getFontMetrics().getHeight();
+        currentHeight += TEXT_LINE_SPACING;
+
+        // Events
+        graphics.setColor(TEXT_COLOR_SECONDARY);
+        graphics.setFont(new Font ("Sanserif", Font.PLAIN, TEXT_SIZE));
+        graphics.drawString(
+                "Total: " + fullEventList.size() + " (Avg: " + format3(fullEventList.size() / 366.0) + " / day)",
+                WINDOW_PADDING + TEXT_INDENT,
+                currentHeight
+        );
+        currentHeight += graphics.getFontMetrics().getHeight();
+        currentHeight += TEXT_LINE_SPACING;
+
+        // Solo Events
+        graphics.setColor(TEXT_COLOR_SECONDARY);
+        graphics.setFont(new Font ("Sanserif", Font.PLAIN, TEXT_SIZE));
+        graphics.drawString(
+                "Solo: " + soloEventList.size() + " (" + formatP(soloEventList.size() / (double) fullEventList.size()) + "%)",
+                WINDOW_PADDING + TEXT_INDENT,
+                currentHeight
+        );
+        currentHeight += graphics.getFontMetrics().getHeight();
+        currentHeight += TEXT_LINE_SPACING;
+
+        // Shared Events
+        graphics.setColor(TEXT_COLOR_SECONDARY);
+        graphics.setFont(new Font ("Sanserif", Font.PLAIN, TEXT_SIZE));
+        graphics.drawString(
+                "Shared: " + sharedEventList.size() + " (" + formatP(sharedEventList.size() / (double) fullEventList.size()) + "%)",
+                WINDOW_PADDING + TEXT_INDENT,
+                currentHeight
+        );
+        currentHeight += graphics.getFontMetrics().getHeight();
+        currentHeight += TEXT_LINE_SPACING;
+
+        // Virtual Events
+        graphics.setColor(TEXT_COLOR_SECONDARY);
+        graphics.setFont(new Font ("Sanserif", Font.PLAIN, TEXT_SIZE));
+        graphics.drawString(
+                "Virtual: " + virtualEventList.size() + " (" + formatP(virtualEventList.size() / (double) fullEventList.size()) + "%)",
+                WINDOW_PADDING + TEXT_INDENT,
+                currentHeight
+        );
+        currentHeight += graphics.getFontMetrics().getHeight();
+        currentHeight += TEXT_LINE_SPACING;
+    }
 
 
+    /*--- Private Formatting Methods ---*/
+
+    private String formatP(double value) {
+        return DECIMAL_FORMAT_P.format(value * 100);
+    }
+
+    private String format3(double value) {
+        return DECIMAL_FORMAT_3.format(value);
+    }
 
 
     /*--- Private UI Methods ---*/
